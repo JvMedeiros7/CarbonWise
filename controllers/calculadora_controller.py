52e1b47ddc99f439370eecb9295b51857bcc96ca
laurabrayner <laurabekinha2@gmail.com>
2026-06-08 16:27:34 -0300

    feat: implementado o motor de calculo co2

--- controllers/calculadora_controller.py
+++ controllers/calculadora_controller.py
@@ -7,8 +7,16 @@ Ficará responsável pela regra de negócio matemática de conversão.
 O desafio é garantir que a conversão de litros e kWh para toneladas de carbono (tCO2e)
 esteja exata e pronta para ser chamada por outras funções (como a de Relatórios da Pessoa 5).
 """
+FATORES_EMISSAO = {
+    "energia": 0.08,     # kg CO2e por kWh
+    "combustivel": 2.68, # kg CO2e por Litro de diesel
+    "transporte": 0.3,   # kg CO2e por km rodado
+    "residuos": 0.5,     # kg CO2e por kg de lixo
+    "agua": 0.00034      # kg CO2e por Litro de água
+}
 
-def calcular_co2(litros_combustivel: float, kwh_energia: float) -> float:
+
+def calcular_co2(kwh_energia: float, litros_combustivel: float, km_transporte: float, kg_residuos: float, litros_agua: float) -> float:
     """
     Realiza o cálculo de emissões de CO2 equivalentes em toneladas.
     
@@ -33,4 +41,15 @@ def calcular_co2(litros_combustivel: float, kwh_energia: float) -> float:
     # emissao_kwh = kwh_energia * FATOR_ENERGIA
     # total_toneladas = (emissao_litros + emissao_kwh) / 1000
     
-    pass
+    kg_energia = kwh_energia * FATORES_EMISSAO["energia"]
+    kg_combustivel = litros_combustivel * FATORES_EMISSAO["combustivel"]
+    kg_transporte = km_transporte * FATORES_EMISSAO["transporte"]
+    kg_residuos = kg_residuos * FATORES_EMISSAO["residuos"]
+    kg_agua = litros_agua * FATORES_EMISSAO["agua"]
+
+    total_kg = kg_energia + kg_combustivel + kg_transporte + kg_residuos + kg_agua
+    total_toneladas = total_kg / 1000
+
+    return total_toneladas
+
+ 
